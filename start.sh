f9f33ddcf83a013eb4f8aced95041ec71e4d3ce1
Alan Kuo <161139744+brotatokuo@users.noreply.github.com>
2025-06-12 13:12:52 -0700

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
+cd ghost-local
 ghost start
+
+# Ping every minute and log it
+while true; do
+    echo "[keepalive] Pinging https://statuskuo.onrender.com at $(date)"
+    curl -s https://statuskuo.onrender.com > /dev/null
+    sleep 60
+done
